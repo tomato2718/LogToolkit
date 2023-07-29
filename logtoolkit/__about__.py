ade39fd6494a075ec64f581da7981fbff1fac571
tomato2718 <qweasdzxc030@gmail.com>
2023-07-30 00:56:01 +0800

    Chore: Update to v1.0.0

--- logtoolkit/__about__.py
+++ logtoolkit/__about__.py
@@ -13,14 +13,22 @@ __all__ = [
 ]
 
 __project__ = 'LogToolkit'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = 'yveschen2718@gmail.com'
 __maintainer__ = 'yveschen2718@gmail.com'
-__release__ = '2023/07/09'
-__summary__ = 'Python project template'
-__usage__ = f'''
+__release__ = '2023/07/30'
+__summary__ = 'A toolkit let you set up logging easier.'
+__usage__ = '''
 Usage:
-    >>> 
-    >>> 
-    >>> 
+    >>> from logtoolkit import quickConfig 
+    >>> logger = quickConfig(
+            name = __name__,
+            formatter='COLOR',
+            stream_handler='stdout',
+            file_handlers={
+                'log_file': './logtoolkit.log',
+                'error_log': './logtoolkit.error.log'
+            }
+        )
+    >>> logger.info('Hello, World!')
 '''
--- logtoolkit/__init__.py
+++ logtoolkit/__init__.py
@@ -1,7 +1,23 @@
+'''
+A toolkit let you set up logging easier.
+'''
+
+__all__ = [
+    'types',
+    'models',
+    'quickConfig',
+    'getLogger',
+    'yamlConfig',
+    'ColorfulFormatter',
+]
+
 from . import types
 from . import models
 from ._logtoolkit import (
     getLogger,
     quickConfig,
     yamlConfig,
+)
+from ._formatters import (
+    ColorfulFormatter
 )
